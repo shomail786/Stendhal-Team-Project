aab17bba01d8ce757865a07d7097fe0b8b1c1148
Maximilian Burnell <maximilian.burnell@student.manchester.ac.uk>
2021-10-27 16:11:13 +0100

    changed the tests so that they can pass, made a erro before and broke them into 4 tests, 2 for each centaur and 1 for each susceptability also added a variable to hold the susceptability in a double

--- tests/games/stendhal/server/entity/creature/CentaurSuceptabilityTest.java
+++ tests/games/stendhal/server/entity/creature/CentaurSuceptabilityTest.java
@@ -1,44 +1,35 @@
 
 package games.stendhal.server.entity.creature;
 
-
-
-import static org.junit.Assert.assertTrue;
-
-import org.junit.BeforeClass;
-import org.junit.Test;
-
-
 import games.stendhal.common.constants.Nature;
 import games.stendhal.server.core.engine.SingletonRepository;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
-import games.stendhal.server.maps.MockStendlRPWorld;
 
-
-
-public class CentaurSuceptabilityTest {
-		
-	/**
-	 * initialisation
-	 */
-	@BeforeClass
-	public static void setUpBeforeClass() {
-		MockStendlRPWorld.get();
-	}
-
+public class CentaurSuceptabilityTest {	
 	@Test
-	public void testSolarCentaurSuceptability(){
-
-			assertTrue(SingletonRepository.getEntityManager().getCreature("solar centaur").getSusceptibility(Nature.FIRE) <= 1);
-			assertTrue(SingletonRepository.getEntityManager().getCreature("solar centaur").getSusceptibility(Nature.ICE) > 1);
-			
-			
+	public void testSolarCentaurFireSuceptability() {
+			double fireSept = SingletonRepository.getEntityManager().getCreature("solar centaur").getSusceptibility(Nature.FIRE);
+			assertEquals(fireSept, 0.8, 0.0);
+					
 	}
-	public void testGlacierCentaurSuceptability(){
-
-		assertTrue(SingletonRepository.getEntityManager().getCreature("glacier centaur").getSusceptibility(Nature.ICE)<= 1);
-		assertTrue(SingletonRepository.getEntityManager().getCreature("glacier centaur").getSusceptibility(Nature.FIRE)> 1);
-		
-		
+	@Test 
+	public void testSolarCentaurIceSuceptability() {
+		double iceSept = SingletonRepository.getEntityManager().getCreature("solar centaur").getSusceptibility(Nature.ICE);
+		assertEquals(iceSept, 1.2, 0.0);
+				
+}
+	
+	@Test
+	public void testGlacierCentaurFireSuceptability() {
+		double fireSept = SingletonRepository.getEntityManager().getCreature("glacier centaur").getSusceptibility(Nature.FIRE);
+		assertEquals(fireSept, 1.2, 0.0);
+	}
+	
+	@Test
+	public void testGlacierCentaurIceSuceptability() {
+		double iceSept = SingletonRepository.getEntityManager().getCreature("glacier centaur").getSusceptibility(Nature.ICE);
+		assertEquals(iceSept, 0.8, 0.0);
 	}
 }
