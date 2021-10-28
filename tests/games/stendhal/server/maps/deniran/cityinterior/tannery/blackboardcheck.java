b47ab319f58029720d12d22ead8dbd18a5918a93
g48534lw <leijing.wang@student.manchester.ac.uk>
2021-10-28 19:54:41 +0800

    Change the test

--- tests/games/stendhal/server/maps/deniran/cityinterior/tannery/blackboardcheck.java
+++ tests/games/stendhal/server/maps/deniran/cityinterior/tannery/blackboardcheck.java
@@ -1,18 +1,16 @@
 package games.stendhal.server.maps.deniran.cityinterior.tannery;
 
-import java.awt.Point;
-
 import org.junit.Assert;
 import org.junit.Test;
 
 import games.stendhal.server.core.engine.StendhalRPZone;
-import games.stendhal.server.entity.mapstuff.sign.ShopSign;
+import games.stendhal.server.entity.Entity;
 import games.stendhal.server.maps.deniran.cityinterior.potionsshop.PotionsDealerNPC;
 import utilities.ZonePlayerAndNPCTestImpl;
 
 
 public class blackboardcheck extends ZonePlayerAndNPCTestImpl{
-	private static final String ZONE_NAME = "int_denerian_potionsshop";
+	private static final String ZONE_NAME = "test zone";
 	
 	
 	public blackboardcheck() {
@@ -26,17 +24,10 @@ public class blackboardcheck extends ZonePlayerAndNPCTestImpl{
 		PotionsDealerNPC zoneconfig = new PotionsDealerNPC();
 		StendhalRPZone testzone = new StendhalRPZone(ZONE_NAME);
 		zoneconfig.configureZone(testzone,null);
-		ShopSign bb1 =zoneconfig.sign1;
-		ShopSign bb2 =zoneconfig.sign2;
-		Point signa = bb1.getOrigin();
-		Point signb = bb2.getOrigin();
-		Assert.assertNotNull(signa);
-		Assert.assertNotNull(signb);
-
-		
-		
-		
-		
+		Entity signa = testzone.getEntityAt(10.0,6.0);
+		Entity signb = testzone.getEntityAt(5.0,6.0);
+		Assert.assertEquals("blackboards",signa);
+		Assert.assertEquals("blackboards",signb);
 	}
 	
 }
